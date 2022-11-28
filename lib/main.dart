fe17371bc991768bb4a34143e26aa088d0d77be4
Pratham <prathamlal20@gmail.com>
2022-11-28 19:18:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:chat_app/shared/Constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: Constants.apiKey, 
+        appId: Constants.appId, 
+        messagingSenderId: Constants.messagingSenderId, 
+        projectId: Constants.projectId
+      )
+    );
+  }
+
+  else{
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId="1:1001726250047:web:840fe412bf32c695c63c8a", 
+    apiKey="AIzaSyBSYA47dNd3nsYq-TgPJBQeFX-YZdNlf9I",
+    messagingSenderId="1001726250047",
+    projectId="chatapp-8195d";
+}
